a0b79da61ce4ae90d6b99adb9b89e7ed362152aa
AhmadRifqi86 <arifadh2003@gmail.com>
2024-08-15 03:53:14 +0000

    add udr_util.go

--- lightcore/NFs/smf/internal/util/udr_util.go
+++ lightcore/NFs/smf/internal/util/udr_util.go
@@ -1,9 +1,24 @@
 package util
 
 import (
+	"encoding/json"
 	"strings"
+
+	"github.com/free5gc/smf/internal/logger"
 )
 
 func EscapeDnn(dnn string) string {
 	return strings.ReplaceAll(dnn, ".", "_")
+}
+
+func UnescapeDnn(dnnKey string) string {
+	return strings.ReplaceAll(dnnKey, "_", ".")
+}
+
+func MapToByte(data map[string]interface{}) []byte {
+	ret, err := json.Marshal(data)
+	if err != nil {
+		logger.UtilLog.Error(err)
+	}
+	return ret
 }
